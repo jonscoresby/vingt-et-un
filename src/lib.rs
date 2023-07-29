c51e3f007553a42acbf237ca4bbfd18f1d254416
Jonathan Scoresby <jonathan@scoresby.tech>
2023-07-28 23:27:06 -0600

    add split funcitonality and general refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use rand::seq::SliceRandom;
-use std::collections::HashSet;
 
 fn create_shoe(size: u8) -> Vec<u8> {
     let mut vec = Vec::new();
@@ -10,154 +9,192 @@ fn create_shoe(size: u8) -> Vec<u8> {
     vec
 }
 
-pub struct Player {
-    pub hand: Vec<u8>,
-    status: HandStatus,
+#[derive(Eq, PartialEq, Hash)]
+pub enum Action {
+    Hit,
+    Stand,
+    Deal,
+    Split,
 }
 
-impl Player {
-    fn deal_new_hand(&mut self, shoe: &mut Vec<u8>) {
-        self.hand.clear();
-        self.deal_card(shoe);
-        self.deal_card(shoe);
-    }
+#[derive(PartialEq, PartialOrd, Copy, Clone)]
+pub enum HandStatus {
+    Value,
+    Bust,
+    Win,
+    Push,
+    Lose,
+    Blackjack,
+}
 
-    fn new() -> Player {
-        Player {
-            hand: Vec::new(),
-            status: HandStatus::Value(0),
+#[derive(PartialEq, Debug)]
+pub enum RoundStatus {
+    Concluded,
+    InProgress(usize),
+}
+
+pub struct Hand {
+    pub cards: Vec<u8>,
+    pub status: HandStatus,
+    pub value: u8,
+}
+
+impl Hand {
+    fn new() -> Hand {
+        Hand {
+            cards: Vec::new(),
+            status: HandStatus::Value,
+            value: 0,
         }
     }
 
-    fn deal_card(&mut self, shoe: &mut Vec<u8>) -> &HandStatus {
-        self.hand.push(shoe.pop().unwrap());
+    fn split(&mut self, shoe: &mut Vec<u8>) -> Hand {
+        let mut new_hand = Hand {
+            cards: vec![self.cards.pop().unwrap()],
+            status: HandStatus::Value,
+            value: 0,
+        };
+        self.deal_card(shoe);
+        new_hand.deal_card(shoe);
+        new_hand
+    }
+
+    fn deal_card(&mut self, shoe: &mut Vec<u8>) -> u8 {
+        self.cards.push(shoe.pop().unwrap());
 
         let mut aces: u8 = self
-            .hand
+            .cards
             .iter()
             .filter(|&n| *n == 1)
             .count()
             .try_into()
             .unwrap();
-        let mut base_sum = aces * 10 + self.hand.iter().sum::<u8>();
+        self.value = aces * 10 + self.cards.iter().sum::<u8>();
 
-        while base_sum > 21 && aces > 0 {
-            base_sum -= 10;
+        while self.value > 21 && aces > 0 {
+            self.value -= 10;
             aces -= 1;
         }
-        self.status = if base_sum > 21 {
-            HandStatus::Bust
-        } else {
-            HandStatus::Value(base_sum)
-        };
-        &self.status
+
+        if self.value > 21 {
+            self.status = HandStatus::Bust
+        }
+
+        self.value
     }
 
-    fn is_blackjack(&self) -> bool {
-        self.status == HandStatus::Value(21)
+    fn is_splittable(&self) -> bool {
+        self.cards.len() == 2 && self.cards[0] == self.cards[1]
     }
 }
 
-#[derive(PartialEq, PartialOrd)]
-enum HandStatus {
-    Value(u8),
-    Bust,
-}
-
-#[derive(PartialEq, Debug)]
-pub enum RoundStatus {
-    Concluded(RoundOutcome),
-    InProgress,
-}
-
-#[derive(PartialEq, Debug)]
-pub enum RoundOutcome {
-    Win,
-    Lose,
-    Push,
-    Blackjack,
-}
-
-pub struct Game {
+pub struct Table {
     shoe: Vec<u8>,
-    possible_actions: HashSet<Action>,
-    pub player: Player,
-    pub dealer: Player,
+    pub player: Vec<Hand>,
+    pub dealer: Hand,
     pub status: RoundStatus,
 }
 
-impl Game {
-    pub fn new() -> Game {
-        let mut game = Game {
+impl Table {
+    pub fn new() -> Table {
+        let mut game = Table {
             shoe: Vec::new(),
-            player: Player::new(),
-            dealer: Player::new(),
-            possible_actions: HashSet::new(),
-            status: RoundStatus::InProgress,
+            player: vec![Hand::new()],
+            dealer: Hand::new(),
+            status: RoundStatus::Concluded,
         };
         game.take_action(Action::Deal);
         game
     }
 
     pub fn take_action(&mut self, action: Action) {
-        match action {
-            Action::Deal => {
+        if let RoundStatus::InProgress(active_hand_index) = self.status {
+            let active_hand = &mut self.player[active_hand_index];
+            match action {
+                Action::Stand => self.next_hand(),
+                Action::Hit => {
+                    if active_hand.deal_card(&mut self.shoe) >= 21 {
+                        self.next_hand();
+                    }
+                }
+                Action::Split => {
+                    if active_hand.is_splittable() {
+                        let new_hand = active_hand.split(&mut self.shoe);
+                        self.player.insert(active_hand_index + 1, new_hand);
+                    } else {
+                        todo!("can't split right now")
+                    }
+                }
+                Action::Deal => todo!("can't deal right now"),
+            }
+        } else {
+            if let Action::Deal = action {
                 if self.shoe.len() < 20 {
                     self.shoe = create_shoe(4);
                 }
-                self.possible_actions.clear();
-                self.dealer.deal_new_hand(&mut self.shoe);
-                self.player.deal_new_hand(&mut self.shoe);
+                self.dealer = self.new_hand();
+                self.player = vec![self.new_hand()];
 
-                self.status = match (
-                    (&mut (self.player)).is_blackjack(),
-                    self.dealer.is_blackjack(),
-                ) {
-                    (true, true) => RoundStatus::Concluded(RoundOutcome::Push),
-                    (true, false) => RoundStatus::Concluded(RoundOutcome::Blackjack),
-                    (false, true) => RoundStatus::Concluded(RoundOutcome::Lose),
-                    (false, false) => RoundStatus::InProgress,
-                };
-                if self.status == RoundStatus::InProgress {
-                    self.possible_actions.insert(Action::Hit);
-                    self.possible_actions.insert(Action::Stand);
-                } else {
-                    self.possible_actions.insert(Action::Deal);
-                }
-            }
-            Action::Hit => {
-                self.player.deal_card(&mut self.shoe);
-                match self.player.status {
-                    HandStatus::Value(_) => (),
-                    HandStatus::Bust => self.status = RoundStatus::Concluded(RoundOutcome::Lose),
-                }
-            }
-            Action::Stand => {
-                while self.dealer.status < HandStatus::Value(17) {
-                    self.dealer.deal_card(&mut self.shoe);
-                }
-                self.status = match self.dealer.status {
-                    HandStatus::Bust => RoundStatus::Concluded(RoundOutcome::Win),
-                    HandStatus::Value(d) => {
-                        if let HandStatus::Value(p) = self.player.status {
-                            match p.cmp(&d) {
-                                std::cmp::Ordering::Less => RoundStatus::Concluded(RoundOutcome::Lose),
-                                std::cmp::Ordering::Equal => RoundStatus::Concluded(RoundOutcome::Push),
-                                std::cmp::Ordering::Greater => RoundStatus::Concluded(RoundOutcome::Win),
-                            }
-                        } else {
-                            RoundStatus::InProgress
-                        }
+                self.player[0].status = match (&self.player[0].status, &self.dealer.status) {
+                    (HandStatus::Blackjack, HandStatus::Blackjack) => {
+                        self.dealer.status = HandStatus::Push;
+                        HandStatus::Push
                     }
+                    (HandStatus::Blackjack, _) => HandStatus::Blackjack,
+                    (_, HandStatus::Blackjack) => HandStatus::Lose,
+                    _ => {
+                        self.status = RoundStatus::InProgress(0);
+                        self.player[0].status
+                    }
+                };
+            } else {
+                todo!("invalid action. start game with deal");
+            }
+        }
+    }
+
+    fn new_hand(&mut self) -> Hand {
+        let mut hand = Hand::new();
+
+        hand.deal_card(&mut self.shoe);
+        if hand.deal_card(&mut self.shoe) == 21 {
+            hand.status = HandStatus::Blackjack
+        }
+
+        hand
+    }
+
+    fn next_hand(&mut self) {
+        if let RoundStatus::InProgress(i) = self.status {
+            self.status = self.get_new_round_status(i + 1);
+        }
+    }
+
+    fn get_new_round_status(&mut self, i: usize) -> RoundStatus {
+        if i == self.player.len() {
+            self.dealer_turn();
+            RoundStatus::Concluded
+        } else if self.player[i].value == 21 {
+            self.get_new_round_status(i + 1)
+        } else {
+            RoundStatus::InProgress(i)
+        }
+    }
+
+    fn dealer_turn(&mut self) {
+        while self.dealer.value < 17 {self.dealer.deal_card(&mut self.shoe);}
+        for hand in &mut self.player {
+            if let HandStatus::Value = hand.status {
+                hand.status = if let HandStatus::Value = self.dealer.status {
+                    match hand.value.cmp(&self.dealer.value) {
+                        std::cmp::Ordering::Less => HandStatus::Lose,
+                        std::cmp::Ordering::Equal => HandStatus::Push,
+                        std::cmp::Ordering::Greater => HandStatus::Win,
+                    }
+                } else {
+                    HandStatus::Win
                 }
             }
-        };
+        }
     }
-}
-
-#[derive(Eq, PartialEq, Hash)]
-pub enum Action {
-    Hit,
-    Stand,
-    Deal,
 }
